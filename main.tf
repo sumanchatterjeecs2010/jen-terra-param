9c60318d09477de14e073ff9ddb40cb001569710
EC2 Default User <ec2-user@ip-172-31-1-147.ap-south-1.compute.internal>
2023-02-21 16:42:18 +0000

    First commit

--- main.tf
+++ main.tf
@@ -23,24 +23,6 @@ resource "aws_instance" "ec2_public" {
   
  
 }
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_private" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
-  subnet_id                   = aws_subnet.private-subnet-1.id
-  associate_public_ip_address = false
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PRIVATE"
-  }
- 
-}
 ################################
  
 
