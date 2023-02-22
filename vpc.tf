ddc7b2b5fa036d5640b068d7ab5f588826834e46
EC2 Default User <ec2-user@ip-172-31-1-147.ap-south-1.compute.internal>
2023-02-22 04:00:25 +0000

    11th  Commit

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 # Create VPC
 # terraform aws create vpc
 resource "aws_vpc" "vpc" {
-  cidr_block           = vpc-cidr
+  cidr_block           = var.vpc-cidr
   instance_tenancy     = "default"
   enable_dns_hostnames = true
   tags = {
